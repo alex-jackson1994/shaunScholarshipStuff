9467a9837aa748e2830e2ba6ebb295a70bbcb300
Shaun Barker <shaun.barker@student.adelaide.edu.au>
2015-12-15 12:22:06 +1030

    made a script to split fasta and ms files

---
+++ splitScript.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+import sys
+from tkinter.filedialog import askopenfilename
+
+if len(sys.argv) > 1:
+    filename = sys.argv[1] #take filename as a system argument
+else:
+	filename = askopenfilename() #pop up box to choose file to load in
+	
+with open(filename) as infile: #lets python realise a variable called infile exists without loading it into memory!
+	j=0
+	for line in infile: #loop over each line in the file
+		if ">" in line:
+			firstSpace=line.find(' ')
+			fileName=line[1:firstSpace]
+			#iterate to next text file and write line if it's the header of a sequence
+			j=j+1
+			print(line, file=open(fileName, 'a'), end='')
+		else:
+			#write line to file if it's just a continuing part of the sequence
+			print(line, file=open(fileName, 'a'), end='')
